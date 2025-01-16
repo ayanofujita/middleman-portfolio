7129b8b1d6b17c9e3390ba6bcf123c7130ebf632
Ayano Fujita <cathy.ayano@gmail.com>
2025-01-16 10:03:13 +0900

    new sections

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,14 +1,14 @@
 // This is where it all goes :)
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('mousemove', (e) => {
-    let images100 = document.querySelectorAll('#banner img.opacity-100:not(:first-child)')
-    const images75 = document.querySelectorAll('#banner img.opacity-75')
-    const images50 = document.querySelectorAll('#banner img.opacity-50')
+    let images100 = document.querySelectorAll('.logo-container img.opacity-100:not(:first-child)')
+    const images75 = document.querySelectorAll('.logo-container img.opacity-75')
+    const images50 = document.querySelectorAll('.logo-container img.opacity-50')
 
     const windowCenterX = window.innerWidth / 2;
     const windowCenterY = window.innerHeight / 2;
 
-    let aws = document.querySelector('#banner img.opacity-100:first-child')
+    let aws = document.querySelector('.logo-container img.opacity-100:first-child')
     const moveX = (e.clientX - windowCenterX) / 80;
     const moveY = (e.clientY - windowCenterY) / 80;
     aws.style.transform = `translate(${moveX}px, ${moveY}px)`;
@@ -34,11 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // const skillButton = document.getElementById('skills-btn');
   // skillButton.addEventListener('click', () => {
-  //   const logos = document.querySelectorAll('#banner img');
+  //   const logos = document.querySelectorAll('.logo-container img');
   //   logos.forEach((logo) => {
   //     logo.classList.toggle('d-none');
   //   })
-  //   const logoContainer = document.querySelector('#banner .img-container');
+  //   const logoContainer = document.querySelector('.logo-container .img-container');
   //   logoContainer.classList.toggle('d-none');
   // });
 });
