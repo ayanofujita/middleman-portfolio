b4f362f899689a8e24fcc8e458eaf0afaa04e257
Ayano Fujita <cathy.ayano@gmail.com>
2025-02-11 13:20:04 +0900

    projects animation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,14 +1,14 @@
 // This is where it all goes :)
 document.addEventListener('DOMContentLoaded', () => {
 
-  // GSAP Animations!
+  // skill-cards
   gsap.registerPlugin(ScrollTrigger);
-  gsap.to("#scroll-text", {
-    scrollTrigger: {
-      trigger: "#skills",
-      scrub: true
-    },
-    duration: 0.1, opacity: 0})
+  // gsap.to("#scroll-text", {
+  //   scrollTrigger: {
+  //     trigger: "#skills",
+  //     scrub: true
+  //   },
+  //   duration: 0.1, opacity: 0})
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: "#skills",
@@ -19,4 +19,41 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   tl.from(".skill-card", {duration: 1, opacity: 0, y: 300, stagger: 1})
+
+  // projects-header
+  gsap.to(".projects-header", {
+    scrollTrigger: {
+      trigger: ".projects-header",
+      start: "top center",
+      endTrigger: ".projects",
+      end: "center center",
+      scrub: true,
+      pin: true
+    }
+  });
+
+  // project card stacking
+  const cardsContainer = document.querySelector('.projects');
+  const cards = document.querySelectorAll('.project-card-wrapper');
+
+  cards.forEach((card, index) => {
+    const offsetTop = 20 + index * 20;
+    card.style.paddingTop = `${offsetTop}px`;
+
+    if (index === cards.length - 1) return;
+
+    const toScale = 1 - (cards.length - 1 - index) * 0.1;
+    const cardInner = card.querySelector('.project-card');
+
+    gsap.to(cardInner, {
+      scale: toScale,
+      scrollTrigger: {
+        trigger: card,
+        ease: "power2.out",
+        start: "center center",
+        end: "bottom center",
+        scrub: true
+      }
+    });
+  });
 });
